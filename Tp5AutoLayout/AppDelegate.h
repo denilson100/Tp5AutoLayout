95619a8eb21b45a2a82ac34b51e413cb4e775810
denilson100 <denilson100@gmail.com>
2016-06-29 18:59:53 -0300

    Auto Layout implementado. Exceto o texto.

---
+++ Tp5AutoLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tp5AutoLayout
+//
+//  Created by Denilson Monteiro on 29/06/16.
+//  Copyright © 2016 infnet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tp5AutoLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tp5AutoLayout
+//
+//  Created by Denilson Monteiro on 29/06/16.
+//  Copyright © 2016 infnet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
